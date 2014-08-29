e62e7dcfc0a74226170285aa96b92d442029660c
Sam Parkinson <sam.parkinson3@gmail.com>
2014-08-29 18:33:25 -0400

    Don't include 'featured' in the activity list

--- web/_plugins/update_json.rb
+++ web/_plugins/update_json.rb
@@ -21,12 +21,14 @@ module Jekyll
         j = JSON.parse open(path).read
         bundle_id = /\.\/data\/(.*).json/.match(path)[1]
 
-        activities[bundle_id] = {
-          "minSugarVersion" => j["minSugarVersion"],
-          "version"         => j["version"],
-          "xo_url"          => j["xo_url"],
-          "xo_size"         => j["xo_size"]
-        }
+        if bundle_id != "featured"
+          activities[bundle_id] = {
+            "minSugarVersion" => j["minSugarVersion"],
+            "version"         => j["version"],
+            "xo_url"          => j["xo_url"],
+            "xo_size"         => j["xo_size"]
+          }
+        end
       end
 
       return {"activities"=>activities}.to_json
