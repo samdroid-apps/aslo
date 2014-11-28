2247156c602c0ec1faf5e08cbbd820f17abe3357
Sam Parkinson <sam.parkinson3@gmail.com>
2014-11-29 07:35:33 +1100

    Remove _site directory
    The _site directory just adds noise to the commits and is 100% auto
    generated anyway.

--- web/_site/data/convert.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open('data.json') as f:
-    j = json.load(f)['activities']
-
-for bid in j:
-    with open(bid + '.json', 'w') as f:
-        json.dump(j[bid], f, indent=4, sort_keys=True)
